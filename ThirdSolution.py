b9e9dd6c22bb7d9ce751054ad7078d0616ab29d5
omar24524 <130411027+omar24524@users.noreply.github.com>
2024-05-05 15:52:54 +0300

    Update ThirdSolution.py

--- ThirdSolution.py
+++ ThirdSolution.py
@@ -1,3 +1,13 @@
+def sortReverse(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key > arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
 def interleave(arr1, arr2):
     if not arr1:
         return arr2
@@ -5,7 +15,6 @@ def interleave(arr1, arr2):
         return arr1
     return [arr2[0]]+interleave(arr2[1:], arr1)
 def wiggleAtya2(arr:list):
-    arr= sorted(arr)#any sorting algorithm
+    arr= sortReverse(arr)#any sorting algorithm
     mid= len(arr)>>1
-    arr= arr[::-1]#reverse, could be done with a loop
     return interleave(arr[:mid], arr[mid:])
